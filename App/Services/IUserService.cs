a025b57a9565741edd784fb68e5b109112adb7bf
kadeksuryam <kadeksuryam@gmail.com>
2022-02-18 03:44:08 +0700

    feat(services): login method

--- App/Services/IUserService.cs
+++ App/Services/IUserService.cs
@@ -1,9 +1,11 @@
 ﻿using App.DTOs.Requests;
+using App.DTOs.Responses;
 
 namespace App.Services
 {
     public interface IUserService
     {
         Task Register(RegisterRequestDTO dto);
+        Task<LoginResponseDTO> Login(LoginRequestDTO dto);
     }
 }
--- App/Services/UserService.cs
+++ App/Services/UserService.cs
@@ -1,6 +1,8 @@
 ﻿namespace App.Services
 {
+    using App.Authorization;
     using App.DTOs.Requests;
+    using App.DTOs.Responses;
     using App.Helpers;
     using App.Models;
     using App.Repositories;
@@ -12,10 +14,15 @@
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UserService(IUserRepository repository, IMapper mapper)
+        private readonly IJwtUtils _jwtUtils;
+        private readonly IBcryptWrapper _bcryptWrapper;
+        public UserService(IUserRepository repository, IMapper mapper, 
+            IJwtUtils jwtUtils, IBcryptWrapper bcryptWrapper)
         {
             _userRepository = repository;
             _mapper = mapper;
+            _jwtUtils = jwtUtils;
+            _bcryptWrapper = bcryptWrapper;
         }
 
         public async Task Register(RegisterRequestDTO dto)
@@ -26,7 +33,7 @@
             }
             var user = _mapper.Map<User>(dto);
 
-            user.EncryptedPassword = BCrypt.HashPassword(dto.Password);
+            user.EncryptedPassword = _bcryptWrapper.hashPassword(dto.Password);
 
             await _userRepository.Add(user);
         }
@@ -34,6 +41,20 @@
         private async Task<bool> IsUsernameTaken(string username)
         {
             return (await _userRepository.Get(username)) != null;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO dto)
+        {
+            User user = await _userRepository.GetByEmail(dto.Email);
+
+            if (user == null || !_bcryptWrapper.isPasswordCorrect(dto.Password, user.EncryptedPassword))
+            {
+                throw new HttpStatusCodeException(HttpStatusCode.Unauthorized, "Incorrect email or password");
+            }
+            
+            var response = _mapper.Map<LoginResponseDTO>(user);
+            response.Token = _jwtUtils.GenerateToken(user);
+            return response;
         }
     }
 
