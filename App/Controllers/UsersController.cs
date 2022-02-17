21c8474ec9239c2ad715aab52538e6836822d9b1
kadeksuryam <kadeksuryam@gmail.com>
2022-02-18 03:44:39 +0700

    feat(controllers): endpoint to login

--- App/Controllers/UsersController.cs
+++ App/Controllers/UsersController.cs
@@ -1,9 +1,12 @@
-﻿using App.DTOs.Requests;
+﻿using App.Authorization;
+using App.DTOs.Requests;
+using App.DTOs.Responses;
 using App.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("users")]
     public class UsersController : ControllerBase
@@ -15,12 +18,28 @@ namespace App.Controllers
             _userService = userService;
         }
 
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequestDTO dto)
         {
             await _userService.Register(dto);
             return Ok(new { message = "Registration successful" });
         }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO reqDTO)
+        {
+            LoginResponseDTO resDTO = await _userService.Login(reqDTO);
+            return Ok(resDTO);
+        }
+
+        [HttpGet]
+        public IActionResult TestAuth()
+        {
+            return Ok(new { message = "success" });
+        }
+
     }
 
 }
