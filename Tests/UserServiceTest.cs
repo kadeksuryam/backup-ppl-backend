ac0e4aebd86382927223206342fcd8a9c8a7271e
kadeksuryam <kadeksuryam@gmail.com>
2022-02-18 03:46:13 +0700

    test: unit test for login method on user services, refactor register method test

--- Tests/UserServiceTest.cs
+++ Tests/UserServiceTest.cs
@@ -1,5 +1,6 @@
-﻿using App.Data;
+﻿using App.Authorization;
 using App.DTOs.Requests;
+using App.DTOs.Responses;
 using App.Helpers;
 using App.Models;
 using App.Repositories;
@@ -13,25 +14,30 @@ namespace Tests
 {
     public class UserServiceTest
     {
+
         [Fact]
         public async void RegisterUser_EmailTaken_ReturnsException()
         {
             // Arrange
+            var mockUserRepo = new Mock<IUserRepository>();
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            var mapper = new Mapper(mapperConfig);
+            var mockJwtUtil = new Mock<IJwtUtils>();
+            var mockBcryptWrapper = new Mock<IBcryptWrapper>();
+
             string username = "test";
-           RegisterRequestDTO dto = new RegisterRequestDTO();
+            RegisterRequestDTO dto = new RegisterRequestDTO();
             dto.Username = username;
 
             User user = new User();
             user.UserName = username;
+            mockUserRepo.Setup(p => p.Get(username)).ReturnsAsync((User)user);
 
-            var mockUserRepo = new Mock<IUserRepository>();
-            mockUserRepo.Setup(p => p.Get(username)).ReturnsAsync(user);
-            var mockMapper = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile(new AutoMapperProfile());
-            });
-            var mapper = mockMapper.CreateMapper();
-            var userService = new UserService(mockUserRepo.Object, mapper);
+            var userService = new UserService(mockUserRepo.Object, mapper,
+                mockJwtUtil.Object, mockBcryptWrapper.Object);
 
             // Act & Assert
             HttpStatusCodeException exception = await Assert.ThrowsAsync<HttpStatusCodeException>(() => userService.Register(dto));
@@ -47,6 +53,15 @@ namespace Tests
         public async void RegisterUser_ValidData_ReturnsSuccess()
         {
             // Arrange
+            var mockUserRepo = new Mock<IUserRepository>();
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            var mapper = new Mapper(mapperConfig);
+            var mockJwtUtil = new Mock<IJwtUtils>();
+            var mockBcryptWrapper = new Mock<IBcryptWrapper>();
+
             string username = "test";
             string password = "testPassword";
             string email = "test@test.com";
@@ -58,23 +73,119 @@ namespace Tests
             dto.Email = email;
             dto.Name = name;
 
-            var mockUserRepo = new Mock<IUserRepository>();
             mockUserRepo.Setup(p => p.Get(username)).ReturnsAsync((User)null);
-            var mockMapper = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile(new AutoMapperProfile());
-            });
-            var mapper = mockMapper.CreateMapper();
-            var user = mapper.Map<User>(dto);
 
-            var userService = new UserService(mockUserRepo.Object, mapper);
+            var userService = new UserService(mockUserRepo.Object, mapper,
+               mockJwtUtil.Object, mockBcryptWrapper.Object);
 
             // Act
-             userService.Register(dto);
+            userService.Register(dto);
 
             // Assert
             mockUserRepo.Verify(p => p.Get(username), Times.Once());
             mockUserRepo.Verify(r => r.Add(It.IsAny<User>()), Times.Once());
+        }
+
+        [Fact]
+        public async void LoginUser_UserNotFound_ReturnsException()
+        {
+            // Arrange
+            var mockUserRepo = new Mock<IUserRepository>();
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            var mapper = new Mapper(mapperConfig);
+            var mockJwtUtil = new Mock<IJwtUtils>();
+            var mockBcryptWrapper = new Mock<IBcryptWrapper>();
+
+            LoginRequestDTO dto = new LoginRequestDTO();
+            dto.Email = "test@test.com";
+            dto.Password = "test";
+
+            mockUserRepo.Setup(p => p.GetByEmail(dto.Email)).ReturnsAsync((User)null);
+            var userService = new UserService(mockUserRepo.Object, mapper,
+                 mockJwtUtil.Object, mockBcryptWrapper.Object);
+
+            // Act & Assert
+            HttpStatusCodeException exception = await Assert.ThrowsAsync<HttpStatusCodeException>(() => userService.Login(dto));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, exception.StatusCode);
+            Assert.Equal("Incorrect email or password", exception.Message);
+            mockUserRepo.Verify(p => p.GetByEmail(dto.Email), Times.Once());
+        }
+
+        [Fact]
+        public async void LoginUser_IncorrectPassword_ReturnsException()
+        {
+            // Arrange
+            var mockUserRepo = new Mock<IUserRepository>();
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            var mapper = new Mapper(mapperConfig);
+            var mockJwtUtil = new Mock<IJwtUtils>();
+            var mockBcryptWrapper = new Mock<IBcryptWrapper>();
+
+            LoginRequestDTO dto = new LoginRequestDTO();
+            dto.Email = "test@test.com";
+            dto.Password = "test";
+            User user = new User();
+            user.EncryptedPassword = "testEncrypt";
+
+            mockUserRepo.Setup(p => p.GetByEmail(dto.Email)).ReturnsAsync((User)user);
+            mockBcryptWrapper.Setup(p => p.isPasswordCorrect(dto.Password, user.EncryptedPassword)).Returns(false);
+
+            var userService = new UserService(mockUserRepo.Object, mapper,
+                mockJwtUtil.Object, mockBcryptWrapper.Object);
+
+            // Act & Assert
+            HttpStatusCodeException exception = await Assert.ThrowsAsync<HttpStatusCodeException>(() => userService.Login(dto));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, exception.StatusCode);
+            Assert.Equal("Incorrect email or password", exception.Message);
+            mockUserRepo.Verify(p => p.GetByEmail(dto.Email), Times.Once());
+
+        }
+
+        [Fact]
+        public async void LoginUser_ValidData_ReturnsSuccess()
+        {
+            // Arrange
+            var mockUserRepo = new Mock<IUserRepository>();
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            var mapper = new Mapper(mapperConfig);
+            var mockJwtUtil = new Mock<IJwtUtils>();
+            var mockBcryptWrapper = new Mock<IBcryptWrapper>();
+
+            LoginRequestDTO dto = new LoginRequestDTO();
+            dto.Email = "test@test.com";
+            dto.Password = "test";
+            User user = new User();
+            user.EncryptedPassword = "testEncrypt";
+            string token = "TOKEN_TEST";
+
+            mockUserRepo.Setup(p => p.GetByEmail(dto.Email)).ReturnsAsync((User)user);
+            mockBcryptWrapper.Setup(p => p.isPasswordCorrect(dto.Password, user.EncryptedPassword)).Returns(true);
+            mockJwtUtil.Setup(p => p.GenerateToken(user)).Returns(token);
+
+            var userService = new UserService(mockUserRepo.Object, mapper,
+                mockJwtUtil.Object, mockBcryptWrapper.Object);
+
+            // Act
+            LoginResponseDTO resDTO = await userService.Login(dto);
+
+            // Assert
+            Assert.NotNull(resDTO.Token);
+            mockUserRepo.Verify(p => p.GetByEmail(dto.Email), Times.Once());
+            mockBcryptWrapper.Verify(p => p.isPasswordCorrect(dto.Password, user.EncryptedPassword), Times.Once());
+            mockJwtUtil.Verify(p => p.GenerateToken(user), Times.Once());
         }
     }
 }
