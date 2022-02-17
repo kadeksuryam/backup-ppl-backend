400eabcbef85376bc68e49447f03ff4334240b7f
kadeksuryam <kadeksuryam@gmail.com>
2022-02-18 03:42:13 +0700

    feat(authorization): add jwt auth middleware and utils

---
+++ App/Authorization/JwtMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using App.Repositories;
+
+namespace App.Authorization
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext ctx, IUserRepository _userRepository, IJwtUtils jwtUtils)
+        {
+            var token = ctx.Request.Headers["Authorization"].FirstOrDefault()?.Split(' ').Last();
+            var userId = jwtUtils.ValidateToken(token);
+            if(userId != null)
+            {
+                ctx.Items["User"] = await _userRepository.GetById(userId.Value);
+            }
+
+            await _next(ctx);
+        }
+    }
+}
