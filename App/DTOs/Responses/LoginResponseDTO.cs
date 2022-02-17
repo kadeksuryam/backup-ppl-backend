69b8f3d664f54945c209452dbd23747ba38f0535
kadeksuryam <kadeksuryam@gmail.com>
2022-02-18 03:40:52 +0700

    feat(DTO): add login req-res DTO

---
+++ App/DTOs/Responses/LoginResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace App.DTOs.Responses
+{
+    public class LoginResponseDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("username")]
+        public string UserName { get; set; }
+        [JsonPropertyName("string")]
+        public string Email { get; set; }
+        [JsonPropertyName("jwt_token")]
+        public string Token { get; set; }
+
+    }
+}
