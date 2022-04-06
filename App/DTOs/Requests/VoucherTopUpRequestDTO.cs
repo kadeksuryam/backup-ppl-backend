e7af754dd2d2c570a8cd83c2fe52c37102901b89
Abdi Haryadi <13519156@std.stei.itb.ac.id>
2022-04-07 04:48:02 +0700

    feat: add JSON attributes for voucher top-up DTOs

--- App/DTOs/Requests/VoucherTopUpRequestDTO.cs
+++ App/DTOs/Requests/VoucherTopUpRequestDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace App.DTOs.Requests
+﻿using System.Text.Json.Serialization;
+
+namespace App.DTOs.Requests
 {
     public class VoucherTopUpRequestDTO
     {
+        [JsonPropertyName("voucher_code")]
         public string VoucherCode { get; set; } = "";
     }
 }
--- App/DTOs/Responses/VoucherTopUpResponseDTO.cs
+++ App/DTOs/Responses/VoucherTopUpResponseDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace App.DTOs.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace App.DTOs.Responses
 {
     public class VoucherTopUpResponseDTO
     {
+        [JsonPropertyName("amount")]
         public uint Amount { get; set; }
     }
 }
