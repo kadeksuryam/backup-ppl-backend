e76a291cc18d937bfd5a1d101fe6c5d51b347854
Abdi Haryadi <13519156@std.stei.itb.ac.id>
2022-04-09 05:12:28 +0700

    feat: add response DTO for get top up histories by user

---
+++ App/DTOs/Responses/GetTopUpHistoriesByUserResponseDTO.cs
@@ -0,0 +1,32 @@
+﻿using App.Helpers.FormatValidator;
+using System.Text.Json.Serialization;
+
+namespace App.DTOs.Responses
+{
+    public class GetTopUpHistoriesByUserResponseDTO
+    {
+        public class TopUpHistoryDTO
+        {
+            [JsonPropertyName("id")]
+            public uint Id { get; set; }
+
+            [JsonPropertyName("created_at")]
+            public string CreatedAt { get; set; } = "";
+
+            [JsonPropertyName("updated_at")]
+            public string UpdatedAt { get; set; } = "";
+
+            [JsonPropertyName("amount")]
+            public int Amount { get; set; }
+
+            [JsonPropertyName("method")]
+            public string Method { get; set; } = "";
+
+            [JsonPropertyName("bank_request_id")]
+            public uint BankRequestId { get; set; }
+
+            [JsonPropertyName("voucher_code")]
+            public string VoucherCode { get; set; } = "";
+        }
+    }
+}
